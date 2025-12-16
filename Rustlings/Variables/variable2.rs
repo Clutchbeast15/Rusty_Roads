f7350d7b885b9de496937d2686bb5124bf177df8
Vaibhav Sutar <sutarvaibhav4518@gmail.com>
2025-12-16 11:33:12 +0530

    Create variable2.rs

---
+++ Rustlings/Variables/variable2.rs
@@ -0,0 +1,21 @@
+/*fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32 = 10;
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}*/
+
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32 = 10; // Initialize x with a value
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
