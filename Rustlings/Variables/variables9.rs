936867d5a568797e7314d8ece45800e4feb95829
Vaibhav Sutar <sutarvaibhav4518@gmail.com>
2025-12-17 11:27:07 +0530

    Implement shadowing examples in variables9.rs

---
+++ Rustlings/Variables/variables9.rs
@@ -0,0 +1,77 @@
+//Shadowing
+//You can declare a new variable with the same name as a previous variable, here we can say the first one is shadowed by the second one.
+
+//🌟🌟
+
+/*
+
+
+// Only modify `assert_eq!` to make the `println!` work(print `42` in terminal)
+fn main() {
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 5);
+    }
+
+    assert_eq!(x, 12);
+
+    let x = 42;
+    println!("{}", x); // Prints "42".
+}
+*/
+
+   /*//////////////////////////////////////////////////////////////
+                                SOLUTION
+  //////////////////////////////////////////////////////////////*/
+// Only modify `assert_eq!` to make the `println!` work(print `42` in terminal)
+fn main() {
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 12);
+    }
+
+    assert_eq!(x, 5);
+
+    let x = 42;
+    println!("{}", x); // Prints "42".
+}
+
+
+//2.🌟🌟
+
+/*
+// Remove a line in the code to make it compile
+fn main() {
+    let mut x: i32 = 1;
+    x = 7;
+    // Shadowing and re-binding
+    let x = x; 
+    x += 3;
+
+
+    let y = 4;
+    // Shadowing
+    let y = "I can also be bound to text!"; 
+
+    println!("Success!");
+}*/
+
+ /*//////////////////////////////////////////////////////////////
+                                SOLUTION
+  //////////////////////////////////////////////////////////////*/
+// Remove a line in the code to make it compile
+fn main() {
+    let mut x: i32 = 1;
+    x = 7;
+    // Shadowing and re-binding
+    let x = x; 
+
+
+    let y = 4;
+    // Shadowing
+    let y = "I can also be bound to text!"; 
+
+    println!("Success!");
+}
