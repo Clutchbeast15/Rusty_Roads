fc3fac31b5e268e7d8c22daf099a9c9de9d04e67
Vaibhav Sutar <sutarvaibhav4518@gmail.com>
2025-12-18 21:27:44 +0530

    Create function5.rs

---
+++ Rustlings/Functions/function5.rs
@@ -0,0 +1,22 @@
+// TODO: Fix the function body without changing the signature.
+fn square(num: i32) -> i32 {
+    num * num;
+}
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {answer}");
+}
+
+  /*//////////////////////////////////////////////////////////////
+                                SOLUTION
+    //////////////////////////////////////////////////////////////*/
+
+fn square(num: i32) -> i32 {
+    num * num // removed the semicolon to make it an expression that returns a value
+}
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {answer}");
+}
